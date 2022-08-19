11bf94ed3f3c652e70729430cc1b2090009511aa
vincentius_ronalto <alto@rubyh.co>
2022-08-19 22:24:07 +0700

    update main.go

--- main.go
+++ main.go
@@ -19,28 +19,41 @@ func main() {
 	var lastInsertId int
 	tx := db.MustBegin()
 
+	// ================= 1) USING tx.QueryRowContext ================= SUCCESS
 	err = tx.QueryRowContext(ctx, `INSERT INTO users (name) VALUES ($1) RETURNING id`, "user1").Scan(&lastInsertId)
 
 	if err != nil {
 		fmt.Println(err)
 		tx.Rollback()
 	}
-	
-	//lastInsertId :  24
+
+	//lastInsertId :  29
 	fmt.Println("lastInsertId : ", lastInsertId)
 
-	result, err := tx.ExecContext(ctx, `INSERT INTO users (name) VALUES ($1)`, "user2")
+	// ================= 2) USING tx.GetContext ================= SUCCESS
+	var lastInsertId2 int
+	err = tx.GetContext(ctx, &lastInsertId2, `INSERT INTO users (name) VALUES ($1) RETURNING id`, "user2")
+
 	if err != nil {
 		fmt.Println(err)
 		tx.Rollback()
 	}
-	
 
-	id, errGetLastID := result.LastInsertId()
+	//lastInsertId :  30
+	fmt.Println("lastInsertId2 : ", lastInsertId2)
+
+	// ================= 3) USING tx.ExecContext ======================= FAILED
+	result, err := tx.ExecContext(ctx, `INSERT INTO users (name) VALUES ($1)`, "user3")
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+	}
+
+	lastInsertId3 , errGetLastID := result.LastInsertId()
 	if errGetLastID != nil {
 		fmt.Println(err)
 	}
 	//lastInsertId2 :  0 -> cannot get since lastInserId not implemented in psql -> https://github.com/jmoiron/sqlx/issues/154
-	fmt.Println("lastInsertId2 : ", id)
+	fmt.Println("lastInsertId3 : ", lastInsertId3)
 	tx.Commit()
 }
